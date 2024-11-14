7e2b62808906783c2d877a57dcf1e52dfde32d3a
drahmetacik <a.acik@icloud.com>
2024-11-14 22:28:54 +0000

    add create and detail views for blog posts, update URL routing

--- myblogrestapi/blogapi/urls.py
+++ myblogrestapi/blogapi/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts/', views.get_posts, name='get_posts'),
+    path('posts/create/', views.create_post, name='create_post'),
+    path('posts/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- myblogrestapi/blogapi/views.py
+++ myblogrestapi/blogapi/views.py
@@ -5,12 +5,46 @@ from .models import BlogPost
 from .serializers import BlogPostSerializer
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def index(request):
     return Response({"message": "Welcome to the blog API!"})
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def get_posts(request):
     get_posts = BlogPost.objects.all()
     serializer = BlogPostSerializer(get_posts, many=True)
     return Response(serializer.data, status=200)
+
+
+@api_view(["POST"])
+def create_post(request):
+    serializer = BlogPostSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def post_detail(request, pk):
+    try:
+        post = BlogPost.objects.get(pk=pk)
+    except BlogPost.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = BlogPostSerializer(post)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = BlogPostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    elif request.method == "DELETE":
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
