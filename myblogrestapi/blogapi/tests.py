c1370bb6519efd1452be56d6d96f803728dd2be9
drahmetacik <a.acik@icloud.com>
2024-11-14 22:07:32 +0000

    add blog post serializer, API view, and tests for retrieving posts

--- myblogrestapi/blogapi/tests.py
+++ myblogrestapi/blogapi/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+import requests
+
+class TestBlogApi(TestCase):
+    def test_get_posts(self):
+        url = 'http://127.0.0.1:8000'
+        request = requests.get(url)
+        self.assertEqual(request.status_code, 200)
+        # {'message': 'Welcome to the blog API!'}
+        self.assertEqual(request.json(), {'message': 'Welcome to the blog API!'})
+        
+    request = requests.get('http://127.0.0.1:8000')
+    print(request.json())
+        
+    
 
 # Create your tests here.
--- myblogrestapi/blogapi/views.py
+++ myblogrestapi/blogapi/views.py
@@ -1,6 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import BlogPost
+from .serializers import BlogPostSerializer
+
 
 @api_view(['GET'])
 def index(request):
     return Response({"message": "Welcome to the blog API!"})
+
+@api_view(['GET'])
+def get_posts(request):
+    get_posts = BlogPost.objects.all()
+    serializer = BlogPostSerializer(get_posts, many=True)
+    return Response(serializer.data, status=200)
